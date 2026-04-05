cec54b128df5b824b47e0571663feefd49be84b7
UrTalent117 <urtalentbl@outlook.com>
2026-04-05 09:39:54 +0800

    [200~3, done.

--- script.lua
+++ script.lua
@@ -7,10 +7,10 @@ clear()
 local obj = obj_new()
 
 -- Add a line component
-obj_component(obj, "Line", 100, 100, 700, 500)
+obj_component(obj, "Line", 100, 100, 700, 500, 255, 0, 0)
 
 -- Add a circle component
-obj_component(obj, "Circle", 400, 300, 100)
+obj_component(obj, "Circle", 400, 300, 100, 0, 255, 0)
 
 -- Draw all components
 obj_drawAll(obj)
